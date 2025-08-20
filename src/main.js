6209fea587ca807fe9ee1ec1f3a5a90155a6c7cb
PcGamer <gogoedy@hotmail.com>
2025-08-20 10:10:19 -0500

    Instlacion  de primevue y de tailwindcss
    Para esto ... y poder dar un compoente mas limpio

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import "./assets/style.css";
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
+
+// Componentes primevue
+import Button from "primevue/button";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue);
+
+// Registrar Componentes primevue
+app.component("Button", Button);
 
 app.mount("#app");
